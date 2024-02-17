67c67b0b0091ab3beef4e8649b84202020b3f844
Elvis Pachacama <davis-elvis@hotmail.com>
2024-02-17 11:36:12 -0500

    Iniciando el proyecto celular

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaControlTallerCelular.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
